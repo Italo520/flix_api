e795a8f9518fd514c97f1714e13533028694fcc2
Italo <italo520@gmail.com>
2025-07-17 13:12:51 -0300

    Serializer.py e ClassAPIview para listar genero implementada

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from generos.views import criar_listar_generos_view, detalhe_genero_view
+from generos.views import GeneroListaCriarAPIView, detalhe_genero_view
 
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('generos/',criar_listar_generos_view, name= 'criar-listar-generos'),
+    path('generos/',GeneroListaCriarAPIView.as_view(), name= 'criar-listar-generos'),
     path('generos/<int:pk>/',detalhe_genero_view , name='detalhe-genero-view'),
 ]
--- generos/views.py
+++ generos/views.py
@@ -3,20 +3,30 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404 
 from generos.models import Genero
+from rest_framework import generics
+from generos.serializers import GeneroSerializer
 
 
-@csrf_exempt
-def criar_listar_generos_view(request):
-    if request.method == 'GET':
-        generos = Genero.objects.all()
-        dados = [{'id': genero.id, 'nome': genero.nome} for genero in generos]
-        return JsonResponse(dados,safe=False)
+
+
+class GeneroListaCriarAPIView(generics.ListCreateAPIView):
+    queryset = Genero.objects.all()
+    serializer_class = GeneroSerializer
+
+
+
+# @csrf_exempt
+# def criar_listar_generos_view(request):
+#     if request.method == 'GET':
+#         generos = Genero.objects.all()
+#         dados = [{'id': genero.id, 'nome': genero.nome} for genero in generos]
+#         return JsonResponse(dados,safe=False)
     
-    elif request.method == 'POST':
-        dados = json.loads(request.body.decode('utf-8'))
-        novo_genero = Genero(nome=dados['nome'])
-        novo_genero.save()
-        return JsonResponse({'id': novo_genero.id, 'nome': novo_genero.nome},status=201,)
+#     elif request.method == 'POST':
+#         dados = json.loads(request.body.decode('utf-8'))
+#         novo_genero = Genero(nome=dados['nome'])
+#         novo_genero.save()
+#         return JsonResponse({'id': novo_genero.id, 'nome': novo_genero.nome},status=201,)
     
 
 @csrf_exempt
