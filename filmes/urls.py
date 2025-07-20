78df623cb07dc64426e83658c31d280b017bd60a
Italo <italo520@gmail.com>
2025-07-19 21:03:05 -0300

    iniciando endpoint de statisticas

--- filmes/urls.py
+++ filmes/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
 
     path('filmes/', views.FilmeListCreateAPIView.as_view(), name='criar-listar-filmes'),
     path('filmes/<int:pk>/', views.FilmeDetailAPIView.as_view(), name='detalhe-filme-view'),
+    path('filmes/stats/', views.FilmeStatsAPIView.as_view(), name='filme-stats-view'),
 
 ]
